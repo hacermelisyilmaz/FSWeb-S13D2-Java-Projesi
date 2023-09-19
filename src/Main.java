2761d9269bdfa982409d4d8894710ee1e58912e8
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-09-19 19:42:37 +0300

    isPalindrome function is completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Palindrom Sayıyı Bulma:");
+        System.out.println(isPalindrome(-1221));
+        System.out.println(isPalindrome(11212));
+        System.out.println("**********");
+    }
+
+    public static boolean isPalindrome(int number) {
+        number = Math.abs(number);
+        int originalNumber = number;
+        int reversedNumber = 0;
+
+        while (number > 0) {
+            int digit = number % 10;
+            number /= 10;
+            reversedNumber = 10 * reversedNumber + digit;
+        }
+
+        return (originalNumber == reversedNumber);
     }
 }
